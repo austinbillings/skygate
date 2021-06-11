47b4476e14ede920c5c03817253e56956423a942
austinbillings <austin@austinbillings.com>
2021-06-10 23:33:29 -0400

    sessions: unsign cookies?

--- src/Sessions.js
+++ src/Sessions.js
@@ -16,7 +16,7 @@ const Sessions = {
     const request = new Request(req);
 
     const token = uid.v4();
-    const signed = true;
+    const signed = false;
     const ip = request.getIp();
     const user = Utils.vetUser(rawUser);
     const start = (new Date()).getTime();
